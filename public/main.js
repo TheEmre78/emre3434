76e814397fa984ef79ba63b3a4025774f5d4acdd
Glitch (socketio-chat) <none>
2019-10-28 14:42:07 +0000

    👦👴 Checkpoint ./public/index.html:5756321/115 ./public/main.js:5756321/681

--- public/main.js
+++ public/main.js
@@ -30,7 +30,7 @@ $(function() {
   function addParticipantsMessage (data) {
     var message = '';
     if (data.numUsers === 1) {
-      message += "there's 1 participant";
+      message += "1 kişi var";
     } else {
       message += "there are " + data.numUsers + " participants";
     }
